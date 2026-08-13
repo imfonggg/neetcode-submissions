85194686a2e1be0ad6957b3c82035deb191c8aaa
Phong Lien <166152872+imfonggg@users.noreply.github.com>
2026-08-13 15:23:58 -0400

    Add: count-paths - submission-0

---
+++ Data Structures & Algorithms/count-paths/submission-0.py
@@ -0,0 +1,15 @@
+class Solution:
+    def uniquePaths(self, m: int, n: int) -> int:
+        cache = [[-1] * n for _ in range(m)]
+        def dp(r, c):
+            if r >= m or c >= n:
+                return 0
+            if cache[r][c] != -1:
+                return cache[r][c]
+            if r == m - 1 and c == n - 1:
+                return 1
+            
+            cache[r][c] = dp(r + 1,c) + dp(r, c + 1)
+            return cache[r][c]
+
+        return dp(0,0)
